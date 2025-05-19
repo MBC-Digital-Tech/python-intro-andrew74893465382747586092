2f5f171e82b560d227752f7ff0215e799585005a
andrew74893465382747586092 <ah23037@mbc.school.nz>
2025-05-19 15:14:17 +1200

    not done yet

--- starter_activity_sheet_3.py
+++ starter_activity_sheet_3.py
@@ -5,15 +5,16 @@ def cooking():
     print()
 
     # change these to your favourite meals
-    print("1. Chicken curry ")
-    print("2. Veggie lasagne")
-    print("3. Burger and salad")
+    print("1. Chicken")
+    print("2. Pork chops")
+    print("3. Burger and chips")
+    print("4. Pizza")  
     # add one more meal here
     print()
     # adapt the next line to add in the 4.
-    print("Which of these meals is your favourite? (1, 2 or 3) ")
+    
     # combine the line above and below into one command that accepts an integer instead of a string.
-    answer = input()
+    answer = input("Which of these meals is your favourite? (1, 2, 3 or 4) ")
     # adapt the if else block to include the fourth meal and compares integers instead of strings.
     if answer == "1":
         print("Chicken curry coming up")
