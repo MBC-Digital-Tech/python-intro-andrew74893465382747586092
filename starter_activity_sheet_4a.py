9624ec43fcb69b96bc936fe9efd9c66f2ea2328a
andrew74893465382747586092 <ah23037@mbc.school.nz>
2025-05-20 12:14:33 +1200

    Task four turtle

--- starter_activity_sheet_4a.py
+++ starter_activity_sheet_4a.py
@@ -8,18 +8,68 @@ from turtle import *
 # remember to use this for turtle graphics in Python
 
 
-def mystery():
-    color('green', 'red')
+def square():
+    color('orange', 'blue')
     begin_fill()
-    forward (100) 
+    forward (200) 
     right (90)
-    forward (100) 
+    forward (200) 
     right (90) 
-    forward (100) 
+    forward (200) 
     right (90) 
-    forward (100)
+    forward (200)
     right (90)
     end_fill()
-    done()
+    done() 
 
-mystery()
+
+def triangle():
+    color('orange', 'blue')
+    begin_fill()
+    forward (200) 
+    right (120)
+    forward (200) 
+    right (120) 
+    forward (200) 
+    right (120)
+    end_fill()
+    penup()
+    right (180)
+    forward (200)
+    pendown()
+    forward (200) 
+    right (90)
+    forward (200) 
+    right (90) 
+    forward (200) 
+    right (90) 
+    forward (200)
+    right (90)
+    end_fill()
+    
+def circle_inside_circle():
+    color('orange', 'blue')
+    begin_fill()
+    circle(10)
+    penup()
+    setposition(0,-70)
+    pendown()
+    color('blue', 'orange')
+    circle(90)
+    end_fill()
+
+def pentagon():
+    color('orange', 'blue')
+    begin_fill()
+    for i in range(6):
+        speed(99999)
+        forward (200) 
+        right (120)
+        forward (200) 
+        right (120) 
+        forward (200) 
+        right (120)
+        right (60)
+    end_fill()
+
+pentagon()
